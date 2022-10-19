01cf3262eccadac6c737a2984bfb659536fbfe63
gbrissi <gbrissi123@gmail.com>
2022-10-19 16:11:41 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+const form = document.getElementById('contact-form')
+const nameForm = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+
 const formSubmit = () => {
-    let form = document.getElementById('contact-form')
-    alert('Mensagem enviada com sucesso!')
-    form.reset();
+    if(nameForm.value == 0 || email.value == 0 || subject.value == 0 || message.value == 0) {
+        alert('Preencha todos os campos!')
+    } else {
+        alert('Mensagem enviada com sucesso!')
+        form.reset();
+    }
 }
+
